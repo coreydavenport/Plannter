2a3af0c70877246030eafed20d6adcefa5afafa5
Corey Davenport <cairnelan@gmail.com>
2016-04-22 01:30:44 -0400

    User sign up functionality added

--- Plannter/www/js/controllers.js
+++ Plannter/www/js/controllers.js
@@ -18,11 +18,37 @@ angular.module('app.controllers', [])
 
 .controller('signupCtrl', function($scope, $firebaseAuth, $firebaseObject, $location) {
 	var ref = new Firebase("https://plannter.firebaseio.com");
-	console.log(ref);
+	//console.log(ref);
 
 	$scope.authObj = $firebaseAuth(ref);
 	var obj = $firebaseObject(ref);
 
+	$scope.createUser = function(form) {
+		console.log("Email: " + form.email);
+		console.log(form.password);
+
+		var info = {
+			email: form.email,
+			password: form.password
+		};
+		$scope.authObj.$createUser({
+			email: form.email,
+			password: form.password
+		}).then(function(userData){
+
+			console.log("User " + userData.uid + " created successfully");
+
+			return $scope.authObj.$authWithPassword({
+				email: form.email,
+				password: form.password
+			});
+		}).then(function(authData){
+			console.log("Logged in as ", authData.uid);
+			$location.path('/dashboard');
+		}).catch(function(error) {
+			console.error("Error : ", error);
+		});
+	}
 
 
 })
